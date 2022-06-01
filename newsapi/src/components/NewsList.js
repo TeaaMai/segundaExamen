5cc86b299cb9c5df563bcafee5d2e126e62f09a7
TeaaMai <94010817+TeaaMai@users.noreply.github.com>
2022-05-31 20:50:24 -0500

    .

--- newsapi/src/components/NewsList.js
+++ newsapi/src/components/NewsList.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import NewsItem from './NewsItem';
 
 const NewsList = () => {
 
@@ -9,16 +10,22 @@ const NewsList = () => {
         const getArticles = async () =>{
             const res = await axios.get("https://newsapi.org/v2/everything?q=apple&from=2022-05-31&to=2022-05-31&sortBy=popularity&apiKey=38daf29e5468416fb72a4afa77b6f3d2");
 
+            setArticles(res.data.articles);
             console.log(res);
         }
         getArticles();
-    })
+    }, [])
 
     return(
         <div>
-
+            {articles.map(({title, description, url, ulrToImage}) => (
+                <NewsItem title={title} 
+                description={description} 
+                url={url} 
+                ulrToImage={ulrToImage}/>
+            ))}
         </div>
-    )
+    );
 }
 
 export default NewsList;
